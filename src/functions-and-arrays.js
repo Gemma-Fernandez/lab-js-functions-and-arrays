0028ba9b775089b100738d394634dd03bf6e4651
Gemma Fernandez <gemmazuera@gmail.com>
2024-08-29 18:34:27 +0200

    Lab acabado

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,15 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    if(num1> num2){
+        return num1;
+    }
+    else if(num1<num2){
+        return num2;
+    }
+    else{
+        return num1
+    }
+}
 
 
 
@@ -7,7 +17,21 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0){
+        return null;
+    }
+
+let wordLongest= words[0];
+for(let i=1; i<words.length; i++){
+    if(words[i].length > wordLongest.length){
+        wordLongest= words[i];
+    }
+}
+
+return wordLongest;
+
+}
 
 
 
@@ -15,7 +39,15 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let totalSum= 0;
+    for(let i=0; i<numbers.length; i++){
+        totalSum += numbers[i];
+        }
+
+return totalSum;
+}
+
 
 
 
@@ -23,12 +55,36 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
 
+    if (numbers2.length === 0) {
+        return 0;
+      }
 
-
+    let totalSuma= 0;
+for(let i=0; i<numbers2.length; i++){
+    totalSuma += numbers2[i];
+    }
+return totalSuma/numbers2.length;
+}
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2, wordToSearch) {
+    if (words2.length === 0) {
+        return null;
+      }
+    
+
+  for (let i = 0; i < words2.length; i++) {
+    if (wordToSearch === words2[i]) {
+      return true;
+    }
+    
+  }
+  return false;
+
+  
+}
+
